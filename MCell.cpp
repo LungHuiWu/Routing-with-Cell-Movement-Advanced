06137063e6dc828e8b66db2b33a3fb9ac1116f66
LungHuiWu <b07901019@ntu.edu.tw>
2021-05-23 22:28:36 +0800

    MCell prototype

--- MCell.cpp
+++ MCell.cpp
@@ -1,4 +1,22 @@
 #include <string>
 #include <iostream>
+#include "MCell.h"
 
 using namespace std;
+
+MCell::MCell()
+{
+    NumBlkg = 0;
+    NumPin = 0;
+}
+
+MCell::MCell(string name, int numpin, int numblkg)
+{
+    Name = name;
+    NumPin = numpin;
+    NumBlkg = numblkg;
+}
+
+MCell::~MCell()
+{
+}
--- MCell.h
+++ MCell.h
@@ -1,3 +1,21 @@
 #include <string>
+#include "Pin.h"
+#include "Blkg.h"
 
 using namespace std;
+
+class MCell
+{
+private:
+    string Name;
+    int NumPin;
+    int NumBlkg;
+    Pin* pin1; // linked list
+    Blkg* blkg1; // linked list
+    
+public:
+    MCell();
+    MCell(string, int, int);
+    ~MCell();
+};
+
